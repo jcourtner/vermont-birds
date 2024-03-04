30961039b0625e3aaf9ed788ce0c81c7b47664ef
Jennifer <jcourtner@salesforce.com>
2024-03-03 20:38:51 -0500

    removing logs

--- nest/src/birdSighting/birdSighting.controller.ts
+++ nest/src/birdSighting/birdSighting.controller.ts
@@ -4,14 +4,6 @@ import { BirdSightingService } from './birdSighting.service';
 @Controller('observations')
 export class BirdSightingController {
   constructor(private readonly birdSightingService: BirdSightingService) {}
-  //find a user PUT request, if the user exists, update their record
-  // if user doesn't exist, create a new record...upsert
-  // @Get('recent')
-  // getRecentObs() {
-  //   console.log('inside recent obs');
-  //   const birdFamily = this.birdSightingService.findBirdFamily();
-  //   return birdFamily;
-  // }
   // if I had more time I would create a DTO for the body
   @Post('new')
   submitBirdObs(@Body() body: any) {
